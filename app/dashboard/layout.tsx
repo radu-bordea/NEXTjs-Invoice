17c58f19aebf4031c9aff26374d6c03c8f9d1a47
radu-bordea <radu.bordea.dev@gmail.com>
2026-09-01 18:15:56 +0200

    change position elements in navbar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -14,11 +14,11 @@ export default async function DashboardLayout({
     <>
       <header className="flex justify-between items-center px-6 h-16 border-b border-zinc-200">
         <nav className="flex gap-6 font-medium text-sm">
+          <a href="/dashboard/settings" className="hover:text-purple-700 border-r border-zinc-200 pr-6">
+            Company Profile
+          </a>
           <a href="/dashboard/invoices" className="hover:text-purple-700">
             Invoices
-          </a>
-          <a href="/dashboard/settings" className="hover:text-purple-700">
-            Company Profile
           </a>
         </nav>
         <UserButton />
